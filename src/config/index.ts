8264246bebcf31e0cb903c10a1e3d012ac79e50d
devjuwel792 <developerjuwel792@gmail.com>
2026-05-22 13:00:02 +0600

    feat: add database initialization with user and issues tables

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,4 +16,33 @@ export const pool = new Pool({
 });
 
 
+export const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(20) NOT NULL DEFAULT 'contributor' CHECK (role IN ('contributor','maintainer')),
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    );
 
+    CREATE TABLE IF NOT EXISTS issues(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(150) NOT NULL CHECK (char_length(title) <= 150),
+        description TEXT NOT NULL CHECK (char_length(description) >= 20),
+        type VARCHAR(20) NOT NULL CHECK (type IN ('bug','feature_request')),
+        status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open',
+        'in_progress','resolved')),
+        reporter_id INT NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    );
+
+    `);
+  } catch (error) {
+    console.log(error);
+  }
+};
